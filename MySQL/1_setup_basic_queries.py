7f6d10a6ef2fc601c6e38365ed47e13803aaf9d1
Thor <74827097+thorzambo@users.noreply.github.com>
2022-05-11 12:21:14 +0200

    start MySQL Setup

--- MySQL/1_setup_basic_queries.py
+++ MySQL/1_setup_basic_queries.py
@@ -4,5 +4,19 @@
 # SQL is a required Skill. 
 # MySQL is Designed to Run on a Server.
 
-import mysql
+import mysql.connector
+
+db = mysql.connector.connect (
+      host='localhost',
+      user='thor',
+      passwd = 'Thor561111',
+      database = 'TestDatabase' # made after -> mycursor.execute("CREATE DATABASE TestDatabase") # First query we make will create the database
+)
+
+mycursor = db.cursor()
+
+# First query we make will create the database
+#mycursor.execute("CREATE DATABASE TestDatabase")
+
+#mycursor.execute("CREATE TABLE Person (name VARCHAR(255), age smallint UNSIGNED, personID int PRIMARY KEY AUTO_INCREMENT)")
 
