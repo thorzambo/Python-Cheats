810d4dfc6fc90cb6e4d6612dcfd8b51ba6ec0df0
Thor <74827097+thorzambo@users.noreply.github.com>
2022-05-11 11:05:22 +0200

    Shesh

--- MySQL/1_setup_basic_queries.py
+++ MySQL/1_setup_basic_queries.py
@@ -2,5 +2,7 @@
 # Structure Queries Language.
 # MySQL Database is one of the Most Popular Database.
 # SQL is a required Skill. 
-# Designed to Run on a Server.
+# MySQL is Designed to Run on a Server.
+
+import mysql
 
